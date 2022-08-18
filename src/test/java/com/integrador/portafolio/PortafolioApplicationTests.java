fb6b3aaa6b9d542bb7e73ffc36a70f6fb0488a76
Diego David Almiron <diegodavidalmiron1990@gmail.com>
2022-08-17 21:34:51 -0300

    test

---
+++ src/test/java/com/integrador/portafolio/PortafolioApplicationTests.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.integrador.portafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
